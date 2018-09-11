305d42406ef0a1a346cca343cf4bbd43ee12d1fe
CodingCherry <cherry.alexeeva@gmail.com>
2018-09-11 19:41:14 +0700

    Set Up TodoListViewController

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Alexeeva Ekaterina on 9/11/18.
+//  Copyright © 2018 Alexeeva Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //MARK - Table View Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+}
+
