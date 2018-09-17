2ebccfebd7ef0ffd7ca1fc082bf39109eba0e95f
CodingCherry <cherry.alexeeva@gmail.com>
2018-09-17 18:34:14 +0700

    Create custom data model

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Alexeeva Ekaterina on 9/17/18.
+//  Copyright © 2018 Alexeeva Ekaterina. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
